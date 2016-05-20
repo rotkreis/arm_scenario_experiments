30692d5e4c42ff9aeae30214a23b3b02fa73e7e2
clement-masson <masson.cle@gmail.com>
2016-05-20 17:43:24 +0200

    ajout write to disk

--- scripts/inspect_bag.py
+++ scripts/inspect_bag.py
@@ -3,12 +3,18 @@
 import argparse
 import rosbag
 import rospy
+import rostopic
+
+from sensor_msgs.msg import JointState
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
-parser.add_argument( '-p', '--path', type=str, required=True, help="the path to the record file")
+parser.add_argument('path', type=str, help="the path to the record file")
 args = parser.parse_args(rospy.myargv()[1:])
 
 bag = rosbag.Bag(args.path)
 for topic, msg, t in bag.read_messages():
-    print('message time : ', msg.header.stamp.secs,  format(msg.header.stamp.nsecs, '09d'), topic)
+    print('message time : ', msg.header.stamp.secs,  format(msg.header.stamp.nsecs, '03d'), topic)
+    print(rostopic.get_topic_class(topic))
+    print(str(type(msg)))
+    print(JointState)
 bag.close()
