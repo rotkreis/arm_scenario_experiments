b000d92b7173a83f061bfd6dbb986236d0e6b854
Mathieu-Seurin <matseurin@cegetel.com>
2017-06-08 18:55:39 +0200

    Update readme, adding script post-install

---
+++ after_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/catkin_ws/src/arm_scenario_experiments/model.sdf ~/catkin_ws/src/arm_scenario_simulator/models/DREAM_table
+
+cp ~/catkin_ws/src/arm_scenario_experiments/spawn_objects_example ~/catkin_ws/src/arm_scenario_simulator/scripts
---
+++ process.sh
@@ -0,0 +1,3 @@
+cd ~/catkin_ws;
+./baxter.sh sim;
+roslaunch arm_scenario_simulator baxter_world.launch;
