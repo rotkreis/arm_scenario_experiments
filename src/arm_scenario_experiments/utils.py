8d7b3bb752018e1a4f451c53b9eecd6ffa025ce2
Xinrui Li <rotkreis@gmail.com>
2017-07-06 14:58:39 +0200

    now with erroneous behavior

--- src/arm_scenario_experiments/utils.py
+++ src/arm_scenario_experiments/utils.py
@@ -12,17 +12,20 @@ quat_conj = transformations.quaternion_conjugate
 
 def point2array(point):
     return np.array([point.x, point.y, point.z])
-    
+
 def quat2array(quat):
     return np.array([quat.x, quat.y, quat.z, quat.w])
-    
+
 def quat_rotate(point, quaternion):
     p = np.concatenate((point, [0]))
     res_inter = quat_mul(quaternion,p)
     return quat_mul(res_inter, quat_conj(quaternion))[0:3]
-    
+
 def change_CS(point, translation, quaternion):
     ''' Get the coordinates of a point in frame B from its coordinates in frmae A.
         The translation must be the coordinates expressed in frame A of the origin of frame B
         The rotation must be the active rotation that takes the points (A,B,C) of coordinates {(1,0,0) (0,1,0) (0,0,1)} in frame A to the points (A',B',C') of coordinates {(1,0,0) (0,1,0) (0,0,1)} in frame B, when the two frames origines are superimposed (no translation)'''
     return quat_rotate((point-translation), quat_conj(quaternion))
+
+def change_CS_reverse(point, translation, quaternion):
+    return quat_rotate(-(point - translation), quat_conj(quaternion))
