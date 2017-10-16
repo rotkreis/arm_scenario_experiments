cd29a5ebacc53d81b4be1eb21f2b17af2be8003d
nataliadiaz <diaz.rodriguez.natalia@gmail.com>
2017-10-16 20:07:53 +0200

    better catching exception and printing more information in the common exception rised, and changing position of button in each data sequence

--- src/arm_scenario_experiments/baxter_utils.py
+++ src/arm_scenario_experiments/baxter_utils.py
@@ -17,52 +17,56 @@ def is_real_robot(force = False):
             _real_robot=True
         except rospy.ROSException: # if not, it is probably cause we're using the simulator
             rospy.loginfo("Service /cameras/list does not exist, you're using the simulated robot")
-            _real_robot=False    
+            _real_robot=False
     return _real_robot
 
 
 def IK(limb, position, orientation, seed_positions = None):
     if not isinstance(position, Point): position = Point(*position)
     if not isinstance(orientation, Quaternion): orientation = Quaternion(*orientation)
-    
+
     ns = "ExternalTools/" + limb.name + "/PositionKinematicsNode/IKService"
     iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
     ikreq = SolvePositionIKRequest()
     pose = PoseStamped(
             header=Header(stamp=rospy.Time.now(), frame_id='base'),
             pose=Pose(position,orientation))
-        
+
     ikreq.pose_stamp.append(pose)
     if seed_positions:
         seed = JointState(Header(stamp=rospy.Time.now(), frame_id='base'), limb.joint_names(), seed_positions, [0]*len(seed_positions), [0]*len(seed_positions))
         ikreq.seed_angles.append(seed)
-        
+
     try:
         rospy.wait_for_service(ns, 5.0)
         resp = iksvc(ikreq)
-    except (rospy.ServiceException, rospy.ROSException), e:
+    except (rospy.ServiceException, rospy.ROSException) as e: # ), e
+        print 'pose:', pose
+        print limb,'limb'
+        print 'position;', position
+        print 'orientation', orientation
         rospy.logerr("Service call failed: %s" % (e,))
-        raise Exception
+
+        raise  e #Exception
 
     if (resp.isValid[0]):
         limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
         return limb_joints
     return None
-        
-        
+
+
 def get_ee_position(limb):
     return utils.point2array(limb.endpoint_pose()['position'])
-    
+
 def get_ee_orientation(limb):
     # WARNING #  Unlike the famous transformations.py which denotes quaternions as arrays [w, x, y, z],
     # WARNING #  tf.transformations denotes quaternions as arrays [x, y, z, w], which is the case here !!!!!
     return utils.quat2array(limb.endpoint_pose()['orientation'])
-    
+
 def move_ee_to(limb, position, orientation=None, seed_positions = None):
     if orientation is None: orientation = limb.endpoint_pose()['orientation']
     joints = IK(limb, position, orientation, seed_positions)
-    if joints: 
+    if joints:
         limb.move_to_joint_positions(joints)
         return True
     return False
-
