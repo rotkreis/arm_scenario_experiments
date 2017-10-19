9dc7b0a930fd5b09142de2a978bf0272b3cb7f71
Antonin <antonin.raffin@ensta-paristech.fr>
2017-10-19 14:20:02 +0200

    Reformat files

--- src/arm_scenario_experiments/baxter_utils.py
+++ src/arm_scenario_experiments/baxter_utils.py
@@ -1,23 +1,26 @@
-''' This module contains often used functions relative to baxter, including an IK solver, get limbs state and moving end effectors to a 3D pose '''
+""" This module contains often used functions relative to baxter, including an IK solver, get limbs state and moving end effectors to a 3D pose """
 
 import rospy
-from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, Vector3, Vector3Stamped
-from std_msgs.msg import Header, String, Empty
-from sensor_msgs.msg import JointState
 from baxter_core_msgs.srv import SolvePositionIK, SolvePositionIKRequest
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Header
+
 from . import utils
 
 _real_robot = None
-def is_real_robot(force = False):
+
+
+def is_real_robot(force=False):
     global _real_robot
     if _real_robot is None or force:
         try:
-            rospy.wait_for_service('/cameras/list', timeout=1) # test if the service is available
+            rospy.wait_for_service('/cameras/list', timeout=1)  # test if the service is available
             rospy.loginfo("Service /cameras/list exists, you're using the real robot")
-            _real_robot=True
-        except rospy.ROSException: # if not, it is probably cause we're using the simulator
+            _real_robot = True
+        except rospy.ROSException:  # if not, it is probably cause we're using the simulator
             rospy.loginfo("Service /cameras/list does not exist, you're using the simulated robot")
-            _real_robot=False
+            _real_robot = False
     return _real_robot
 
 
@@ -29,41 +32,45 @@ def IK(limb, position, orientation, seed_positions=None):
     iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
     ikreq = SolvePositionIKRequest()
     pose = PoseStamped(
-            header=Header(stamp=rospy.Time.now(), frame_id='base'),
-            pose=Pose(position, orientation))
+        header=Header(stamp=rospy.Time.now(), frame_id='base'),
+        pose=Pose(position, orientation))
 
     ikreq.pose_stamp.append(pose)
     if seed_positions:
-        seed = JointState(Header(stamp=rospy.Time.now(), frame_id='base'), limb.joint_names(), seed_positions, [0]*len(seed_positions), [0]*len(seed_positions))
+        seed = JointState(Header(stamp=rospy.Time.now(), frame_id='base'), limb.joint_names(), seed_positions,
+                          [0] * len(seed_positions), [0] * len(seed_positions))
         ikreq.seed_angles.append(seed)
 
     try:
         rospy.wait_for_service(ns, 5.0)
         resp = iksvc(ikreq)
-    except (rospy.ServiceException, rospy.ROSException) as e: # ), e
+    except (rospy.ServiceException, rospy.ROSException) as e:  # ), e
         print 'pose:', pose
-        print limb,'limb'
+        print limb, 'limb'
         print 'position;', position
         print 'orientation', orientation
         rospy.logerr("Service call failed: %s" % (e,))
 
         raise
 
-    if (resp.isValid[0]):
+    if resp.isValid[0]:
         limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
         return limb_joints
     return None
 
+
 # ee = end effector ?
 def get_ee_position(limb):
     return utils.point2array(limb.endpoint_pose()['position'])
+
 
 def get_ee_orientation(limb):
     # WARNING #  Unlike the famous transformations.py which denotes quaternions as arrays [w, x, y, z],
     # WARNING #  tf.transformations denotes quaternions as arrays [x, y, z, w], which is the case here !!!!!
     return utils.quat2array(limb.endpoint_pose()['orientation'])
 
-def move_ee_to(limb, position, orientation=None, seed_positions = None):
+
+def move_ee_to(limb, position, orientation=None, seed_positions=None):
     if orientation is None: orientation = limb.endpoint_pose()['orientation']
     joints = IK(limb, position, orientation, seed_positions)
     if joints:
--- src/arm_scenario_experiments/recorder.py
+++ src/arm_scenario_experiments/recorder.py
@@ -1,18 +1,21 @@
 import os
+from copy import copy
+
 import rospy
 import rosbag
-from copy import copy
 import rostopic
+
 
 class Recorder(object):
     def __init__(self, path, prefix, topics):
         self.path = path
-        os.system('mkdir -p '+path)
+        # TODO: Replace this command with os.makedirs(path, exist_ok=True)
+        os.system('mkdir -p ' + path)
         self.prefix = prefix
-        self.topics= topics
+        self.topics = topics
         self.recorded_topics = {}
         self.publishers = {}
-        self.lastMessages = {topic:None for topic in topics}
+        self.lastMessages = {topic: None for topic in topics}
         self.bag = None
 
         self._latches = {}
@@ -24,45 +27,49 @@ class Recorder(object):
     def __del__(self):
         self.close_bag()
 
-
     def new_bag(self, name):
-        if self.bag: raise Excpetion('First close the current bag before creating a new one')
-        bag_name = self.path+'/'+name+'.bag'
-        rospy.loginfo('Creating new bag : '+bag_name)
+        if self.bag:
+            raise Exception('First close the current bag before creating a new one')
+        bag_name = self.path + '/' + name + '.bag'
+        rospy.loginfo('Creating new bag : ' + bag_name)
         self.bag = rosbag.Bag(bag_name, 'w')
 
     def close_bag(self):
         if self.bag:
             print('Closing the bag')
-            while self._bag_latch:pass
+            while self._bag_latch: pass
             self.bag.close()
             self.bag = None
         else:
             rospy.loginfo('No bag currently open')
 
     def new_topic_name(self, topic):
-        prefix= self.prefix
-        if topic[0]!='/' and prefix[-1]!='/': prefix+='/'
-        return prefix+topic
+        prefix = self.prefix
+        if topic[0] != '/' and prefix[-1] != '/': prefix += '/'
+        return prefix + topic
 
     ''' Callback for recording signals'''
+
     def make_callback(self, topic):
         written_topic = self.recorded_topics[topic]
+
         def callback(message):
-            while self._latches[topic]: pass
+            while self._latches[topic]:
+                pass
             self._latches[topic] = True
             self.lastMessages[topic] = message
             self._latches[topic] = False
+
         return callback
 
     def check_topic(self, topic):
-        message_class = rostopic.get_topic_class(topic)[0] # None if topic is not already published
+        message_class = rostopic.get_topic_class(topic)[0]  # None if topic is not already published
         if message_class:
             self.recorded_topics[topic] = self.new_topic_name(topic)
             self._latches[topic] = False
-            rospy.Subscriber(topic, message_class, self.make_callback(topic), queue_size = 10)
-            self.publishers[topic] = rospy.Publisher(self.recorded_topics[topic], message_class, queue_size = 1)
-            rospy.loginfo('create suscriber for '+topic)
+            rospy.Subscriber(topic, message_class, self.make_callback(topic), queue_size=10)
+            self.publishers[topic] = rospy.Publisher(self.recorded_topics[topic], message_class, queue_size=1)
+            rospy.loginfo('create suscriber for ' + topic)
             return True
         return False
 
@@ -79,12 +86,12 @@ class Recorder(object):
 
     def dump(self, topic):
         if not self.bag:
-            raise Exception('Cannot dump '+topic+' : no bag opened')
-        if isinstance(topic,list):
+            raise Exception('Cannot dump ' + topic + ' : no bag opened')
+        if isinstance(topic, list):
             return [self.dump(t) for t in topic]
         if topic not in self.recorded_topics:
             self.check_topic(topic)
-            rospy.loginfo('Checking for '+topic+' again')
+            rospy.loginfo('Checking for ' + topic + ' again')
             return
         if not self.lastMessages[topic]:
             return
